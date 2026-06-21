0d2b2d4d7a35f98bc8f1ad0cea267d33e7937240
Sourav Mantri <mantrisourav@gmail.com>
2026-06-21 23:10:56 +0530

    Add pixel ID

--- app/layout.tsx
+++ app/layout.tsx
@@ -87,7 +87,7 @@ export default function RootLayout({
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', 'YOUR_PIXEL_ID'); 
+            fbq('init', '1721569655665280'); 
             fbq('track', 'PageView');
           `}
         </Script>
